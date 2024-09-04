32590426f2953c9e1cfd6ff6fa477736552fadc5
Santiago Olguín <SantoII1735@gmail.com>
2024-09-04 18:58:39 +0000

    Connnected MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL);
+
+// Schema
+
+const URLPairSchema = new mongoose.Schema({
+  shorturl: { type: String, required: true },
+  original_url: { type: String, required: true }
+});
+
+const URLPair = mongoose.model("URLPair", URLPairSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,7 +29,8 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body)
   res.json({ greeting: 'hello API' });
 });
 
