9bc3b00e17df8b9aa54332acec49b9d6272a7ed1
Santiago Olguín <SantoII1735@gmail.com>
2024-09-04 20:30:50 +0000

    feat: adding urls to db

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
+const dns = require('dns');
+const urlparser = require('url');
+const { doesNotMatch } = require('assert');
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -28,9 +31,22 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
+// Handle POST new Url
 app.post('/api/shorturl', function(req, res) {
-  console.log(req.body)
+  const url = req.body.url;
+
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname, 
+    async (err, address) => {
+      if (!address) {
+        res.json({ error: 'invalid url'});
+      } else {
+        // Insert URL into DB
+        const urlId = await URLPair.countDocuments({});
+        const newEntry = new URLPair({ shorturl: urlId, original_url: url});
+        console.log(newEntry);
+        newEntry.save();
+      }
+    })
   res.json({ greeting: 'hello API' });
 });
 
